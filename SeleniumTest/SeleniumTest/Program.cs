154bba6b6cd7a5986458b6c821d9499412125fc3
Błażej Stanisz <blazej.stanisz@intel.com>
2019-04-24 11:44:39 +0200

    changes

---
+++ SeleniumTest/SeleniumTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SeleniumTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testContainer = new SeleniumTestApi();
+            testContainer.RunTest1();
+        }
+    }
+}
